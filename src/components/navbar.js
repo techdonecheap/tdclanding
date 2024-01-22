382a0b2efc57f923c81bf85aeb58bc95761b1432
cojovi <31863818+cojovi@users.noreply.github.com>
2024-01-22 06:27:55 -0600

    e
    e

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link
                   className="text-white opacity-50 hover:opacity-100 px-3 py-2 rounded-md text-sm font-medium font-poppins"
-                  to="/about"
+                  to="/careers"
                 >
                   About
                 </Link>
