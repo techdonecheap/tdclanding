ce2b94e27441031243a0c727901f04e25c53fa45
cojovi <31863818+cojovi@users.noreply.github.com>
2024-02-14 05:17:31 -0600

    Update blogHeader.js

--- src/components/Blog/blogHeader.js
+++ src/components/Blog/blogHeader.js
@@ -6,21 +6,21 @@ const BlogHeader = () => {
     <Fade bottom>
       <div className="max-w-7xl mx-auto mt-10 flex text-white xxs:flex-col sm:flex-row">
         <div className="h-full object-cover overflow-hidden rounded-xl m-5">
-          <img src="https://i.imgur.com/UGjEabHl.jpg"></img>
+          <img src="https://i.imgur.com/7QC5WBJ.png"></img>
         </div>
         <div className="m-5">
-          <h2 className="text-sm opacity-50">Insights from the Field</h2>
+          <h2 className="text-sm opacity-50">Insights from inside the void</h2>
           <h1 className="text-5xl font-bold font-poppins xxs:text-lg sm:text-5xl">
-            Predator Management Insights You Need to Know
+            Tech Done Cheap Insights You Need to Know
           </h1>
           <p className="text-lg mt-4 opacity-50 xxs:text-sm sm:text-lg">
-            Welcome to the OSPM blog, where expertise meets the art of storytelling. Our blog is more than just words; it's a dive into the heart of predator management, a field where precision, respect for nature, and cutting-edge strategies converge. Since our inception, OSPM has been setting new standards in wildlife control, combining our rich experience with innovative techniques. Join us as we share insights from the field, explore the balance between human needs and environmental stewardship, and unveil the techniques that make OSPM a leader in predator management. This is where knowledge meets action, and where you'll find everything you need to understand the nuances of effective wildlife control..
+            Welcome to the Tech Done Cheap blog, where practicality meets innovation in the realm of affordable technology. Our platform is more than just tips and tricks its a journey into the essence of maximizing technology within any budget. Since we started, Tech Done Cheap has been pioneering ways to make technology accessible and sustainable, blending our deep understanding of tech with cost-effective solutions. Join us as we share insights into economical tech upgrades, DIY projects, and how to leverage technology for personal and business growth without overspending. This blog is where frugality meets functionality, offering everything you need to navigate the tech world affordably.
           </p>
           <div className="flex items-center mt-5">
             <div className="h-10 w-10 object-cover overflow-hidden rounded-full">
               <img
                 className="h-10 w-10"
-                src="https://i.imgur.com/BXHexwpl.png"
+                src="https://i.imgur.com/7QC5WBJ.png"
               ></img>
             </div>
             <div className="ml-2">
